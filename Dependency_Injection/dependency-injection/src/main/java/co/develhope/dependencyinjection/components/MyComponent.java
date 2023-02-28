e53e2417feec7f02dbab076ffef6c94d9590a414
Alessandro Trupia <114166336+AlessandroTrupia@users.noreply.github.com>
2023-02-28 10:20:37 +0100

    Add files via upload

---
+++ Dependency_Injection/dependency-injection/src/main/java/co/develhope/dependencyinjection/components/MyComponent.java
@@ -0,0 +1,20 @@
+package co.develhope.dependencyinjection.components;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyComponent {
+
+    private String myComponentName;
+
+    public MyComponent(){
+        this.myComponentName = "Alessandro Trupia";
+        System.out.println("MyComponent constructor has been called");
+    }
+
+    public String getMyComponentName(){
+        System.out.println("MyComponent.getMyComponentName() has been called");
+        return myComponentName;
+    }
+
+}
