124bc8adb83ce8f3c7e313f21d0b59fb33606616
Alessandro Trupia <114166336+AlessandroTrupia@users.noreply.github.com>
2023-03-13 13:32:02 +0100

    Add files via upload

---
+++ Mock_Database/mock-db/src/main/java/co/develhope/mockdb/entities/Student.java
@@ -0,0 +1,62 @@
+package co.develhope.mockdb.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "students")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    public Student() {
+    }
+
+    public Student(long id, String lastName, String firstName, String email) {
+        this.id = id;
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.email = email;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
